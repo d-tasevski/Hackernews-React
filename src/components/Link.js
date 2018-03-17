9adcdee491625fe487aad653f67f04c5490bce7d
Puritanic <darktasevski@gmail.com>
2018-03-17 18:31:33 +0100

    Final touches

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,19 +1,74 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { AUTH_TOKEN } from '../constants';
+import { timeDifferenceForDate } from '../utils';
 
 class Link extends Component {
   render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN);
     return (
-      <div>
-        <div>
-          {this.props.link.description} ({this.props.link.url})
+      <div className="flex mt2 items-start">
+        <div className="flex items-center">
+          <span className="gray">{this.props.index + 1}.</span>
+          {authToken && (
+            <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
+              ▲
+            </div>
+          )}
+        </div>
+        <div className="ml1">
+          <div>
+            {this.props.link.description} ({this.props.link.url})
+          </div>
+          <div className="f6 lh-copy gray">
+            {this.props.link.votes.length} votes | by{' '}
+            {this.props.link.postedBy
+              ? this.props.link.postedBy.name
+              : 'Unknown'}{' '}
+            {timeDifferenceForDate(this.props.link.createdAt)}
+          </div>
         </div>
       </div>
     );
   }
 
+  // The update function that we’re adding as an argument to the mutation invocation will be called directly after the server returned the response.
+  // It receives the payload of the mutation (data) and the current cache (store) as arguments.
+  // WE can then use this input to determine a new state for the cache.
+
   _voteForLink = async () => {
-    // ... you'll implement this in chapter 6
+    const linkId = this.props.link.id;
+    await this.props.voteMutation({
+      variables: {
+        linkId
+      },
+      update: (store, { data: { vote } }) => {
+        this.props.updateStoreAfterVote(store, vote, linkId);
+      }
+    });
   };
 }
 
-export default Link;
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+export default graphql(VOTE_MUTATION, {
+  name: 'voteMutation'
+})(Link);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloLink, split } from 'apollo-client-preset';
+import { getMainDefinition } from 'apollo-utilities';
 import { ApolloProvider } from 'react-apollo';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } from './constants';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -12,16 +17,53 @@ import registerServiceWorker from './registerServiceWorker';
 // GraphQL server will be running on http://localhost:4000.
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
 
+// Apollo provides a nice way for authenticating all requests by using the concept of middleware, implemented as an Apollo Link.
+// This middleware will be invoked every time ApolloClient sends a request to the server.
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  const authorizationHeader = token ? `Bearer ${token}` : null;
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  });
+  return forward(operation);
+});
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
+// https://www.howtographql.com/react-apollo/8-subscriptions/
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLinkWithAuthToken
+);
+
 // instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache.
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache()
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();
